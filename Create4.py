a855680717a9cc97e482bc592c52854326165d08
Jackson Lewis-Hite <68624065+jacksonlewis97@users.noreply.github.com>
2024-02-11 11:36:52 -0600

    Create Create4.py

---
+++ Create4.py
@@ -0,0 +1,10 @@
+#This is your starter file for the Creating a Quiz project
+#The lines that start with a # are comments and will not show up in your code
+#You should utilize this feature to leave notes about what variables are for
+#And what operations your code is performing
+
+#Try to have your program be inutitive for the user to interact with
+#To help you get started, here are the first two lines of your program (feel free to edit these)
+
+favoriteThing = '' #fill this string with your quiz subject
+print(f'Welcome to the {favoriteThing} quiz!')
